e23a67494804c8e3b4066684f0864e6d8e5e3627
Troy Denton <trdenton@gmail.com>
2015-05-23 01:02:52 -0500

    added serial printing example

--- exercise-3/e3.ino
+++ exercise-3/e3.ino
@@ -2,26 +2,35 @@
  * Millennium Library - Arduino Workshop
  * 
  * TITLE:        Example #3
- * DESCRIPTION:  Read an analog voltage.
+ * DESCRIPTION:  Print to the serial console
  * AUTHOR:       Dario Schor (schor@ieee.org), Troy Denton (trdenton@gmail.com)
  * DATE:         March 22, 2014
  **/ 
 
 
+int switchPin = 7;
+int LEDPin = 13;
+
+int numPushes=0;
 void setup() {
   // put your setup code here, to run once:
+  pinMode(switchPin,INPUT);
+  pinMode(LEDPin,OUTPUT);
+  digitalWrite(LEDPin,LOW);
+  
   Serial.begin(9600);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
+  int switch_value = digitalRead(switchPin);
+  if(switch_value==LOW)
+  {
+   numPushes = numPushes + 1;
+   Serial.print("BUTTON PUSHED ");
+   Serial.print(numPushes);
+   Serial.println(" TIMES!");
+   delay(250);
+  }
 
-    // put your main code here, to run repeatedly:
-    int reading = analogRead(A0);
-    float voltage = reading*(5.0/1024.0);
-
-    Serial.println(voltage);
-  
-    delay(500);
-  
-  
 }
